d20e94ec559a79738d6cf99f8fc5fef3bbf7d690
v.combettes <vivi_c@hotmail.fr>
2024-11-24 17:09:00 +0100

    tooltip config

--- src/pages/Games/GamesArray/GamesArray.tsx
+++ src/pages/Games/GamesArray/GamesArray.tsx
@@ -1,4 +1,5 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, tooltipClasses, TooltipProps } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
 import { useGetGames } from '../../../hooks/queries/games/useGetGames';
 import { DateHelper } from '../../../utils/DateHelper';
 import { PlayersBlock } from '../DrawerGamesForm/DrawerGamesForm';
@@ -6,6 +7,18 @@ import styles from './GamesArray.module.scss';
 
 const GamesArray = () => {
     const { data: games } = useGetGames()
+
+    const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
+        <Tooltip {...props} classes={{ popper: className }} />
+    ))({
+        [`& .${tooltipClasses.tooltip}`]: {
+            maxWidth: 'none',
+            margin: 0,
+            fontSize: '13px',
+            backgroundColor: 'rgb(29, 29, 29)',
+            borderRadius: 8
+        }
+    });
 
     const formatType = (type: string) => {
         if (type === 'team') return 'En équipe'
@@ -27,12 +40,12 @@ const GamesArray = () => {
         else return ''
     }
 
-    const formatConfig = (type: string, config: Array<PlayersBlock>) => (
+    const formatConfig = (victoire: string, type: string, config: Array<PlayersBlock>) =>(
         <>
-            <ul style={{ margin: 0 }}>
+            <ul style={{ margin: 0, listStyle: 'none', padding: 0 }}>
                 {config.map((conf) => {
                     return (
-                        <li>
+                        <li style={{ margin: 10, color: showWinnerStyle(victoire, conf) }}>
                             <strong>Joueur: </strong>{conf.joueur}<strong>, Deck: </strong>{conf.deck}{typeGame(type, conf)}
                         </li>
                     )
@@ -40,6 +53,17 @@ const GamesArray = () => {
             </ul>
         </>
     )
+
+    const showWinnerStyle = (victoire: string, config: PlayersBlock) => {
+        const isWinner = (
+            (config?.userId === victoire) 
+            || (victoire === 'Seigneur' ? (config?.role === victoire || config?.role === 'Gardien') : config?.role === victoire) 
+            || (config?.team === victoire) 
+        )
+
+        if (isWinner) return '#00ff00'
+        return '#FF0033'
+    }
 
     return (
         <>
@@ -49,7 +73,6 @@ const GamesArray = () => {
                         <TableRow>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Date</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Type de partie</TableCell>
-                            <TableCell align="center" style={{ minWidth: "150px" }}>Config</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Victoire</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Type de victoire</TableCell>
                         </TableRow>
@@ -59,8 +82,14 @@ const GamesArray = () => {
                             <TableRow key={game.id}>
                                 <TableCell align="center" component="th" scope="row">{ game?.date ? DateHelper.formatAsFrenchDate(game?.date) : '-' }</TableCell>
                                 <TableCell align="center">{ formatType(game.type) }</TableCell>
-                                <TableCell align="left">{ formatConfig(game.type, game.config) }</TableCell>
-                                <TableCell align="left">{ formatVictoire(game.type, game.victoire, game.config) }</TableCell>
+                                <TableCell align="left">
+                                    <div className={styles.infosDecks}>
+                                        { formatVictoire(game.type, game.victoire, game.config) }
+                                        <CustomTooltip title={formatConfig(game.victoire, game.type, game.config)} placement="top">
+                                            <InfoIcon fontSize="small" color="primary"/>
+                                        </CustomTooltip>
+                                    </div>
+                                </TableCell>
                                 <TableCell align="center">{ game.typeVictoire }</TableCell>
                             </TableRow>
                         ))}
--- src/pages/Profil/HistoryGames/HistoryGames.tsx
+++ src/pages/Profil/HistoryGames/HistoryGames.tsx
@@ -1,13 +1,26 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-import { useGetHistoryGames } from "../../../hooks/queries/games/useGetHistoryGames"
-import { useAppSelector } from "../../../hooks/useAppSelector"
-import { DateHelper } from "../../../utils/DateHelper"
-import { PlayersBlock } from "../../Games/DrawerGamesForm/DrawerGamesForm"
-import styles from './HistoryGames.module.scss'
+import InfoIcon from '@mui/icons-material/Info';
+import { styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, tooltipClasses, TooltipProps } from "@mui/material";
+import { useGetHistoryGames } from "../../../hooks/queries/games/useGetHistoryGames";
+import { useAppSelector } from "../../../hooks/useAppSelector";
+import { DateHelper } from "../../../utils/DateHelper";
+import { PlayersBlock } from "../../Games/DrawerGamesForm/DrawerGamesForm";
+import styles from './HistoryGames.module.scss';
 
 const HistoryGames = () => {
     const { data: gameHistory } = useGetHistoryGames()
     const user = useAppSelector((state) => state.auth.user);
+
+    const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
+        <Tooltip {...props} classes={{ popper: className }} />
+    ))({
+        [`& .${tooltipClasses.tooltip}`]: {
+            maxWidth: 'none',
+            margin: 0,
+            fontSize: '13px',
+            backgroundColor: 'rgb(29, 29, 29)',
+            borderRadius: 8
+        }
+    });
 
     const formatType = (type: string) => {
         if (type === 'team') return 'En équipe'
@@ -27,12 +40,12 @@ const HistoryGames = () => {
         else return ''
     }
 
-    const formatConfig = (type: string, config: Array<PlayersBlock>) => (
+    const formatConfig = (victoire: string, type: string, config: Array<PlayersBlock>) =>(
         <>
-            <ul style={{ margin: 0 }}>
+            <ul style={{ margin: 0, listStyle: 'none', padding: 0 }}>
                 {config.map((conf) => {
                     return (
-                        <li>
+                        <li style={{ margin: 10, color: showWinnerStyle(victoire, conf) }}>
                             <strong>Joueur: </strong>{conf.joueur}<strong>, Deck: </strong>{conf.deck}{typeGame(type, conf)}
                         </li>
                     )
@@ -53,6 +66,17 @@ const HistoryGames = () => {
         return 'red'
     }
 
+    const showWinnerStyle = (victoire: string, config: PlayersBlock) => {
+        const isWinner = (
+            (config?.userId === victoire) 
+            || (victoire === 'Seigneur' ? (config?.role === victoire || config?.role === 'Gardien') : config?.role === victoire) 
+            || (config?.team === victoire) 
+        )
+
+        if (isWinner) return '#00ff00'
+        return '#FF0033'
+    }
+
     return (
         <>
             <TableContainer className={styles.tableau}>
@@ -61,7 +85,6 @@ const HistoryGames = () => {
                         <TableRow>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Date</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Type de partie</TableCell>
-                            <TableCell align="center" style={{ minWidth: "150px" }}>Config</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Victoire</TableCell>
                             <TableCell align="center" style={{ minWidth: "75px" }}>Type de victoire</TableCell>
                         </TableRow>
@@ -70,9 +93,15 @@ const HistoryGames = () => {
                         {gameHistory?.map((game) => (
                             <TableRow key={game.id} className={styles[winnerStyle(game.victoire, game.config)]}>
                                 <TableCell align="center" component="th" scope="row">{ game?.date ? DateHelper.formatAsFrenchDate(game?.date) : '-' }</TableCell>
-                                <TableCell align="center">{ formatType(game.type) }</TableCell>
-                                <TableCell align="left">{ formatConfig(game.type, game.config) }</TableCell>
-                                <TableCell align="left">{ formatVictoire(game.type, game.victoire, game.config) }</TableCell>
+                                <TableCell align="center">{ formatType(game.type) }</TableCell>   
+                                <TableCell align="left">
+                                    <div className={styles.infosDecks}>
+                                        { formatVictoire(game.type, game.victoire, game.config) }
+                                        <CustomTooltip title={formatConfig(game.victoire, game.type, game.config)} placement="top">
+                                            <InfoIcon fontSize="small" color="primary"/>
+                                        </CustomTooltip>
+                                    </div>
+                                </TableCell>
                                 <TableCell align="center">{ game.typeVictoire }</TableCell>
                             </TableRow>
                         ))}

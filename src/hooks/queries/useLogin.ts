a0e2a4c369682021e57c93aabbbf293350fc7734
v.combettes <vivi_c@hotmail.fr>
2025-09-28 08:21:38 +0200

    resetQuery login

--- src/hooks/queries/useLogin.ts
+++ src/hooks/queries/useLogin.ts
@@ -1,9 +1,11 @@
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { authActions, AuthUser } from '../../store/reducers/authReducer.js';
 import { Api } from '../../utils/Api';
 import { DEFAULT_PAGE_PATH } from '../../router/routes.js';
+import { useGetAllPlayers } from './joueurs/useGetAllPlayers.js';
+import { useGetUsersDecks } from './joueurs/useGetUsersDecks.js';
 
 const login = (nom: string, prenom:string, password: string) => (
     new Api<{ token: string, user: AuthUser }>()
@@ -13,13 +15,16 @@ const login = (nom: string, prenom:string, password: string) => (
 export const useLogin = (): UseMutationResult<{ token: string }, Error, { nom: string; prenom: string, password: string }> => {
     const navigate = useNavigate();
     const dispatch = useDispatch()
+    const queryClient = useQueryClient();
+
 
     return useMutation({
         mutationFn: (data: { nom: string; prenom: string,  password: string }) => login(data.nom, data.prenom, data.password),
         onSuccess: (data) => {
-            if (data.user) {
-                dispatch(authActions.updateState(data.user));
-            }
+            useGetAllPlayers.reset(queryClient);
+            useGetUsersDecks.reset(queryClient);
+
+            if (data.user) dispatch(authActions.updateState(data.user));
 
             if (data.token) localStorage.setItem('token', data.token)
             navigate(DEFAULT_PAGE_PATH);

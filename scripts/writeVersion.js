f8e0703d8fe2472714f89d75948065f8fbbd09bd
v.combettes <vivi_c@hotmail.fr>
2025-10-22 21:27:41 +0200

    ca degage

--- scripts/writeVersion.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const buildDir = path.resolve(__dirname, '../build');
-if (!fs.existsSync(buildDir)) fs.mkdirSync(buildDir, { recursive: true });
-
-const version = process.env.VITE_APP_VERSION || '1.0.0';
-fs.writeFileSync(path.join(buildDir, 'version.json'), JSON.stringify({ version }, null, 2));
-console.log('version.json created.');

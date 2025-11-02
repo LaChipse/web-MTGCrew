3fd30beb4ca9c730eaf3ca56daff457613225782
v.combettes <vivi_c@hotmail.fr>
2025-11-02 19:44:27 +0100

    taille modal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.onguo053m3"
+    "revision": "0.ar39tapub3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

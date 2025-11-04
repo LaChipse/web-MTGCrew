3d01b08ff1344512893b05db10e3c540a01b7fd6
v.combettes <vivi_c@hotmail.fr>
2025-11-04 07:06:34 +0100

    correction error sass

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
   plugins: [
     react(),
     VitePWA({
